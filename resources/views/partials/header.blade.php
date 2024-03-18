85e353650f29e91cf5687d38580f1ec1db3ed75d
SerenaMalusa <sere.malu@gmail.com>
2024-03-18 16:54:55 +0100

    feat: add header blue bar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,14 @@
+<div class="bg-primary text-light" id="blue-bar">
+
+    <div class="container d-flex justify-content-end gap-2 text-uppercase">
+        <p class="mb-1">DC POWER™ VISA®</p>
+        <p class="mb-1">
+            ADDITIONAL DC SITES
+            <i class="fa-solid fa-caret-down"></i>
+        </p>
+    </div>
+    
+</div>
 
 <div class="container">
     <h1>header</h1>
