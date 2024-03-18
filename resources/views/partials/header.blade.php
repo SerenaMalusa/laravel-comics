f384999063984b4d6c18a6dbacc3ba09cf95a8ea
SerenaMalusa <sere.malu@gmail.com>
2024-03-18 17:05:07 +0100

    feat: add funtional link in header that go to home and comics

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,15 +1,29 @@
-<div class="bg-primary text-light" id="blue-bar">
-
-    <div class="container d-flex justify-content-end gap-2 text-uppercase">
-        <p class="mb-1">DC POWER™ VISA®</p>
-        <p class="mb-1">
-            ADDITIONAL DC SITES
-            <i class="fa-solid fa-caret-down"></i>
-        </p>
+<header id="header">
+    
+    <div class="bg-primary text-light" id="blue-bar">
+    
+        <div class="container d-flex justify-content-end gap-2 text-uppercase">
+            <p class="mb-1">DC POWER™ VISA®</p>
+            <p class="mb-1">
+                ADDITIONAL DC SITES
+                <i class="fa-solid fa-caret-down"></i>
+            </p>
+        </div>
+        
     </div>
     
-</div>
+    <div class="container d-flex fw-semibold">
+        <a href="{{ route('home') }}">
+            <img src="{{ url('/dc-logo.png') }}" alt="">
+        </a>
+        
+        <nav class="text-uppercase">
+            <ul>
+                <li>
+                    <a href="{{ route('comics') }}">Comics</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
 
-<div class="container">
-    <h1>header</h1>
-</div>
+</header>
